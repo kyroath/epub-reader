bedddc39c25986e1dfd7d7e6298602aee089fdc8
Fatih Karahan <hseyin97@gmail.com>
2020-11-11 21:53:19 +0300

    created the boilerplate electron-react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@material-ui/core/Button";
+import { remote } from "electron";
+
+const showOpenDialog = remote.dialog.showOpenDialog;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={async () => {
+          const { filePaths } = await showOpenDialog({
+            properties: ["openFile"],
+          });
+          console.log(filePaths[0]);
+        }}
+      >
+        Click me!
+      </Button>
     </div>
   );
 }
