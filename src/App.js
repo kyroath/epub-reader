911217dd09bdbccbe0697cecce6ea88bebdbd725
Fatih Karahan <hseyin97@gmail.com>
2020-11-12 12:09:42 +0300

    switched to pnpm and added the addBook button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import Button from "@material-ui/core/Button";
 import { remote } from "electron";
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
 
-const showOpenDialog = remote.dialog.showOpenDialog;
+import Navbar from "./components/Navbar";
+
+const useStyles = makeStyles((theme) => ({
+  addIcon: {
+    "& svg": {
+      fontSize: 300,
+    },
+  },
+  container: {
+    flexDirection: "column",
+    textAlign: "center",
+  },
+  iconContainer: {
+    marginTop: 30,
+    marginBottom: 30,
+    display: "flex",
+    justifyContent: "center",
+  },
+}));
 
 function App() {
+  const [path, setPath] = useState("");
+  const classes = useStyles();
+
   return (
     <div>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={async () => {
-          const { filePaths } = await showOpenDialog({
-            properties: ["openFile"],
-          });
-          console.log(filePaths[0]);
-        }}
-      >
-        Click me!
-      </Button>
+      <Navbar />
+      <Container className={classes.container}>
+        <div className={classes.iconContainer}>
+          <IconButton
+            edge="start"
+            disableRipple
+            className={classes.addIcon}
+            hidden
+            color="grey"
+            aria-label="add"
+            onClick={async () => {
+              const { filePaths } = await remote.dialog.showOpenDialog({
+                properties: ["openFile"],
+              });
+              setPath(filePaths);
+            }}
+          >
+            <AddIcon style={{ justifyContent: "center" }} />
+          </IconButton>
+        </div>
+        <Typography style={{ width: "60%", margin: "auto" }} variant="h5">
+          Uh oh... It looks like you have no books open. Click the button above
+          to add your first book!
+        </Typography>
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 
 import "fontsource-roboto";
+
+import App from "./App";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
